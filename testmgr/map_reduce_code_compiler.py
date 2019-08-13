8399c1e0270eb0766b385fa4cd66b7eec717d913
Nishant <nishantravishankar@gmail.com>
2019-08-13 18:39:26 +0530

    Modified instance variable names to comply with pep-8

--- testmgr/map_reduce_code_compiler.py
+++ testmgr/map_reduce_code_compiler.py
@@ -1,18 +1,18 @@
 import os
 
 class MapReduceCodeCompiler:
-    hadoop_common_jar_path = """/usr/local/hadoop/share/hadoop/common/hadoop-common-2.7.2.jar"""
-    hadoop_client_core_jar_path = """/usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-client-core-2.7.2.jar"""
-    hadoop_client_common_cli_jar_path = """/usr/local/hadoop/share/hadoop/common/lib/commons-cli-1.2.jar"""
-    base_command = "javac -classpath"
+    _hadoop_common_jar_path = """/usr/local/hadoop/share/hadoop/common/hadoop-common-2.7.2.jar"""
+    _hadoop_client_core_jar_path = """/usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-client-core-2.7.2.jar"""
+    _hadoop_client_common_cli_jar_path = """/usr/local/hadoop/share/hadoop/common/lib/commons-cli-1.2.jar"""
+    _base_command = "javac -classpath"
     def compile_code(self,path):
         destination_class_files_path = ("/".join(path.split("/")[:-1]) 
                                         + "/ClassFiles")
         os.mkdir(destination_class_files_path)
-        command = (self.base_command + " "
-                   + self.hadoop_common_jar_path + ":"
-                   + self.hadoop_client_core_jar_path + ":"
-                   + self.hadoop_client_common_cli_jar_path
+        command = (self._base_command + " "
+                   + self._hadoop_common_jar_path + ":"
+                   + self._hadoop_client_core_jar_path + ":"
+                   + self._hadoop_client_common_cli_jar_path
                    + " -d " + destination_class_files_path
                    + " " + path)
         os.system(command)
