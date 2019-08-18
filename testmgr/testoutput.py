ea7ed0055c5242f31e139e2c402000e0824d4971
Nishant <nishantravishankar@gmail.com>
2019-08-18 12:56:59 +0530

    Modified testoutput class to handle multiple test cases

--- testmgr/testoutput.py
+++ testmgr/testoutput.py
@@ -1,4 +1,14 @@
+from config import *
 class TestOutput:
+    def test(self,output_paths):
+        correctness = []
+        for output_path in output_paths:
+            test_case = output_paths.index(output_path) + 1
+            correctness.append(self.check_test_case(output_path, 
+                                                   (SETTERS_OUTPUT_BASE_PATH
+                                                   +str(test_case)
+                                                   +".txt"),
+                                                   str(test_case)))
     def check_test_case(self, path_to_team_output, path_to_correct_output, 
                         test_case_number):
         team_output = open(path_to_team_output,'r').read()
@@ -9,6 +19,3 @@ class TestOutput:
         else:
             print("TEST CASE "+str(test_case_number)+" FAILED")
             return 0
-            
-        
-    
