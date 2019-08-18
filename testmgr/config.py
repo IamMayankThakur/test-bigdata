56d9cb4418ccd12c18e844dd97792bd896c49300
Nishant <nishantravishankar@gmail.com>
2019-08-18 12:55:47 +0530

    Added config.py

---
+++ testmgr/config.py
@@ -0,0 +1,7 @@
+CODE_BASE_PATH = "/home/hduser/Desktop/WordCountF/"
+SETTERS_OUTPUT_BASE_PATH = "/home/hduser/Desktop/BigData/Outputs/"
+TEAMS_BASE_PATH = "/home/hduser/Desktop/BigData/Teams/"
+HADOOP_HOST_NAME = "localhost"
+HADOOP_NAMENODE_PORT_NUMBER = "50070"
+HADOOP_USER_NAME = "hduser"
+TEST_CASES = 2
