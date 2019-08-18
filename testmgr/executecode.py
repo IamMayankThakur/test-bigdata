dced5c3dc6fac7846c7bfb7f6ac5b47a69aa9363
Nishant <nishantravishankar@gmail.com>
2019-08-18 12:56:25 +0530

    Modified execute class to handle multiple test cases

--- testmgr/executecode.py
+++ testmgr/executecode.py
@@ -4,35 +4,44 @@ from mapreducecodecompiler import MapReduceCodeCompiler
 from jarfilegenerator import JarFileGenerator
 from mapreduceexecutor import MapReduceExecutor
 from testoutput import TestOutput
+from config import *
 
 class ExecuteCode:
-    _base_path = "/home/hduser/Desktop/BigData/Teams"
-    def execute(self, assignment_number, path_to_code, team_id, test_case_number):
+    def execute_test_cases(self, assignment_number, path_to_code, team_name):
+        output_paths = []
+        print("TEST_CASES = "+str(TEST_CASES))
+        for test_case in range(1,TEST_CASES+1):
+            output_paths.append(self.execute(assignment_number,
+                                             path_to_code,
+                                             team_name,
+                                             str(test_case)))
+        print("LENGTH OF OUTPUT PATHS = "+str(len(output_paths)))
+        return output_paths
+    def execute(self, assignment_number, path_to_code, team_name, test_case_number):
         if(int(assignment_number) == 1):
-            path = self._base_path + "/Assignment-1/"
+            path = TEAMS_BASE_PATH + "Assignment-1/"
             try:
-                os.mkdir(os.path.join(path,team_id))
+                os.mkdir(os.path.join(path,team_name))
             except:
                 pass
-            os.mkdir(os.path.join(path, team_id, test_case_number))
-            #print(os.path.join(path, team_id, "Outputs", test_case_number))
+            os.mkdir(os.path.join(path, team_name, test_case_number))
             obj_1 = MapReduceCodeCompiler()
-            obj_1.compile_code(path_to_code, os.path.join(path,team_id))
-            print("[TEST-COMPONENT-LOG]["+assignment_number+"/"+team_id+"] COMPILATION SUCCESSFUL")
+            obj_1.compile_code(path_to_code, os.path.join(path, team_name))
+            print("[TEST-COMPONENT-LOG]["+assignment_number+"/"+team_name+"] COMPILATION SUCCESSFUL")
             obj_2 = JarFileGenerator()
-            obj_2.generate_jar(os.path.join(path,team_id,"ClassFiles",),
-                               os.path.join(path,team_id),
+            obj_2.generate_jar(os.path.join(path,team_name,"ClassFiles",),
+                               os.path.join(path,team_name),
                                "WordCountJ")
-            print("[TEST-COMPONENT-LOG]["+assignment_number+"/"+team_id+"] JAR FILE CREATED")
+            print("[TEST-COMPONENT-LOG]["+assignment_number+"/"+team_name+"] JAR FILE CREATED")
             obj_3 = MapReduceExecutor()
-            obj_3.execute(os.path.join(path,team_id,"WordCountJ.jar"),
+            obj_3.execute(os.path.join(path,team_name,"WordCountJ.jar"),
                           "WordCount", "/Test_Case_"+str(test_case_number), "Output/")
-            print("[TEST-COMPONENT-LOG]["+assignment_number+"/"+team_id+"] MAP REDUCE EXECUTION SUCCESSFUL")
+            print("[TEST-COMPONENT-LOG]["+assignment_number+"/"+team_name+"] MAP REDUCE EXECUTION SUCCESSFUL")
             
-            client = InsecureClient('http://localhost:50070', user='hduser')
-            client.download("Output/",os.path.join(path,team_id,test_case_number))
+            client = InsecureClient('http://'+HADOOP_HOST_NAME+':'+HADOOP_NAMENODE_PORT_NUMBER, user=HADOOP_USER_NAME)
+            client.download("Output/",os.path.join(path,team_name,test_case_number))
             client.delete('Output/', recursive=True)
-            return os.path.join(path,team_id,test_case_number,"Output","part-r-00000")        
+            return os.path.join(path,team_name,test_case_number,"Output","part-r-00000")        
             
             
             
@@ -43,14 +52,7 @@ class ExecuteCode:
 def test():
     obj_1 = ExecuteCode()
     obj_2 = TestOutput()
-    correctness = []
-    for test_case in range(1,3):
-        output_path = obj_1.execute('1','/home/hduser/Desktop/WordCountF/Code_'+str(2)+'/WordCount.java','2',str(test_case))
-        correctness.append(obj_2.check_test_case(output_path,
-                                                 "/home/hduser/Desktop/BigData/Outputs/"+str(test_case)+".txt",
-                                                  str(test_case)))
-    print(correctness)
-        
-        
+    output_paths = obj_1.execute_test_cases('1',CODE_BASE_PATH+'Code_'+str(1)+'/WordCount.java','1')
+    correctness = obj_2.test(output_paths)
 
 test()
