05cd1bf3ac0fe8de4eaff2a3e66aec560c82119a
Mayank Thakur <thakurmayank88@gmail.com>
2019-09-02 22:25:15 +0530

    Bug Fixes

--- adminmgr/testmgr/executecode.py
+++ adminmgr/testmgr/executecode.py
@@ -8,6 +8,7 @@ from .config import *
 from .jarfilegenerator import java_jar_file_generator
 from .mapreducecodecompiler import java_map_reduce_compile
 from .mapreduceexecutor import java_map_reduce_execute
+from .mapreduceexecutor import python_map_reduce_execute
 from .testoutput import test
 
 
@@ -47,7 +48,7 @@ def process_java(path_to_code, team_name):
     print("[TEST-COMPONENT-LOG][" + team_name + "] COMPILATION SUCCESSFUL")
 
     if (java_jar_file_generator(os.path.join(team_folder_path, "ClassFiles"),
-                                team_folder_path, "WordCountJ") == False):
+                                team_folder_path, "WordCountJ") is False):
         return -3
     print("[TEST-COMPONENT-LOG][" + "/" + team_name + "] JAR FILE CREATED")
     return team_folder_path
@@ -65,7 +66,7 @@ def java_execute_test_cases(path_to_code, team_name):
 
 
 def python_execute_test_cases(path_to_mapper,
-                              path_to_reducer, 
+                              path_to_reducer,
                               team_name):
     output_paths = []
     team_folder_path = os.path.join(TEAMS_BASE_PATH, team_name)
@@ -84,17 +85,19 @@ def python_execute_test_cases(path_to_mapper,
                                            str(test_case)))
     return output_paths
 
-def execute_python(path_to_mapper, path_to_reducer, team_name, test_case):
-    if(python_map_reduce_execute(path_to_mapper, path_to_reducer, "/Input/data.txt", "/output_word") == False):
+
+def execute_python(path_to_mapper, path_to_reducer, team_folder_path, test_case_number):
+    if (python_map_reduce_execute(path_to_mapper, path_to_reducer, "/Input/alldata.csv", "/output_word") is False):
         return None
     print("[TEST-COMPONENT-LOG]MAP REDUCE EXECUTION SUCCESSFUL")
     download_file(team_folder_path, test_case_number)
     return os.path.join(team_folder_path, test_case_number, "part-00000")
 
+
 def execute_java(path_to_code, team_folder_path, test_case_number):
     if (java_map_reduce_execute(os.path.join(team_folder_path, "WordCountJ.jar"),
                                 "WordCount", "/Test_Case_" + str(test_case_number),
-                                HADOOP_OUTPUT_PATH) == False):
+                                HADOOP_OUTPUT_PATH) is False):
         return None
     print("[TEST-COMPONENT-LOG]MAP REDUCE EXECUTION SUCCESSFUL")
 
@@ -103,9 +106,6 @@ def execute_java(path_to_code, team_folder_path, test_case_number):
 
 
 def exe(submission_id):
-    # TODO: Add another parameter to test function to know which task to evaluate
-    # if python:
-    # else:
     submission = SubmissionAssignmentOne.objects.get(id=submission_id)
     mail_message_1 = "Task 1\n"
     mail_message_2 = "Task 2\n"
@@ -113,97 +113,100 @@ def exe(submission_id):
     score_1 = 0
     score_2 = 0
     score_3 = 0
-    if submission.python:
+    if submission.java:
         output_paths = java_execute_test_cases(submission.code_file_java_task_1.path, submission.team.team_name)
-        if(output_paths == -1):
+        if (output_paths == -1):
             mail_message_1 += "unknown error\n"
-        else if(output_paths == -2):
+        elif (output_paths == -2):
             mail_message_1 += "compilation error for task\n"
-        else if(output_paths == -3):
+        elif (output_paths == -3):
             mail_message_1 += "jar file generation issue\n"
         else:
             correctness = test(output_paths, '1')
             for i in range(len(correctness)):
                 mail_message_1 += "Test case " + str(i) + " "
-                if(correctness[i] == 1):
+                if (correctness[i] == 1):
                     mail_message_1 += "Passed\n"
-                    score_1+=1
+                    score_1 += 1
                 else:
                     mail_message_1 += "Failed\n"
-                
-        
+
         output_paths = java_execute_test_cases(submission.code_file_java_task_1.path, submission.team.team_name)
-        if(output_paths == -1):
+        if (output_paths == -1):
             mail_message_2 += "unknown error"
-        else if(output_paths == -2):
+        elif (output_paths == -2):
             mail_message_2 += "compilation error"
-        else if(output_paths == -3):
+        elif (output_paths == -3):
             mail_message_2 += "jar file generation error"
         else:
             correctness = test(output_paths, '1')
             for i in range(len(correctness)):
                 mail_message_2 += "Test case " + str(i) + " "
-                if(correctness[i] == 1):
+                if (correctness[i] == 1):
                     mail_message_2 += "Passed\n"
-                    score_2+=1
+                    score_2 += 1
                 else:
                     mail_message_2 += "Failed\n"
 
         output_paths = java_execute_test_cases(submission.code_file_java_task_1.path, submission.team.team_name)
         print(output_paths)
-        if(output_paths == -1):
+        if (output_paths == -1):
             mail_message_3 += "unknown error"
-        else if(output_paths == -2):
+        elif (output_paths == -2):
             mail_message_3 += "compilation error for task"
-        else if(output_paths == -3):
+        elif (output_paths == -3):
             mail_message_3 += "jar file generation issue"
         else:
             correctness = test(output_paths, '1')
             for i in range(len(correctness)):
                 mail_message_3 += "Test case " + str(i) + " "
-                if(correctness[i] == 1):
+                if (correctness[i] == 1):
                     mail_message_3 += "Passed\n"
-                    score_3+=1
+                    score_3 += 1
                 else:
                     mail_message_3 += "Failed\n"
-    else:
+    if submission.python:
         output_paths = python_execute_test_cases(submission.code_file_java_task_1.path, submission.team.team_name)
-        if(output_paths[0] == False):
+        if (output_paths[0] == False):
             mail_message_1 += "Compilation error\n"
         else:
             correctness = test(output_paths, '1')
             for i in range(len(correctness)):
                 mail_message_1 += "Test case " + str(i) + " "
-                if(correctness[i] == 1):
+                if (correctness[i] == 1):
                     mail_message_1 += "Passed\n"
-                    score_1+=1
+                    score_1 += 1
                 else:
                     mail_message_1 += "Failed\n"
-                    
+
         output_paths = python_execute_test_cases(submission.code_file_java_task_1.path, submission.team.team_name)
-        if(output_paths[0] == False):
+        if (output_paths[0] == False):
             mail_message_2 += "Compilation error\n"
         else:
             correctness = test(output_paths, '1')
             for i in range(len(correctness)):
                 mail_message_2 += "Test case " + str(i) + " "
-                if(correctness[i] == 1):
+                if (correctness[i] == 1):
                     mail_message_2 += "Passed\n"
-                    score_2+=1
+                    score_2 += 1
                 else:
                     mail_message_2 += "Failed\n"
 
         output_paths = python_execute_test_cases(submission.code_file_java_task_1.path, submission.team.team_name)
-        if(output_paths[0] == False):
+        if (output_paths[0] == False):
             mail_message_3 += "Compilation error\n"
         else:
             correctness = test(output_paths, '1')
             for i in range(len(correctness)):
                 mail_message_3 += "Test case " + str(i) + " "
-                if(correctness[i] == 1):
+                if (correctness[i] == 1):
                     mail_message_3 += "Passed\n"
-                    score_3+=1
+                    score_3 += 1
                 else:
                     mail_message_3 += "Failed\n"
-        
 
+    submission.remarks = mail_message_1 + ';' + mail_message_2 + ';' + mail_message_3
+    submission.score_1 = score_1
+    submission.score_2 = score_2
+    submission.score_3 = score_3
+    submission.save()
