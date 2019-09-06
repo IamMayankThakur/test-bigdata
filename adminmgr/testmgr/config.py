447dae0be70715912d2d6787208ee95936f63216
Mayank Thakur <thakurmayank88@gmail.com>
2019-09-06 12:39:00 +0530

    Changed setter path

--- adminmgr/testmgr/config.py
+++ adminmgr/testmgr/config.py
@@ -5,7 +5,7 @@ HADOOP_NAMENODE_PORT_NUMBER = "9870"
 HADOOP_USER_NAME = "ubuntu"
 HADOOP_STREAMING_BASE_PATH = "/usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.2.0.jar"
 HADOOP_OUTPUT_PATH = "/output_word"
-SETTERS_OUTPUT_BASE_PATH = "/home/ubuntu/test-bigdata/expected/Outputs"
+SETTERS_OUTPUT_BASE_PATH = "/home/ubuntu/test-bigdata/adminmgr/testmgr/Setters_Output"
 
 
 '''CODE_BASE_PATH = "/home/hduser/Desktop/WordCountF"
