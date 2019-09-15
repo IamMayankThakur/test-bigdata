9d1d4784e567fe062d47158985bf9d80bb8def02
Ubuntu <ubuntu@ip-172-31-44-5.ec2.internal>
2019-09-15 17:30:30 +0000

    made video

---
+++ adminmgr/media/code/python/map3/BD_802_816_1835mapper2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+venue = ""
+ven_lis = []
+for line in sys.stdin:
+    line = line.strip()
+    if(line.split(",", 2)[1] == "venue"):
+        line = line.split(",", 2)
+    else:
+        line = line.split(",")
+    try:
+        if(int(line[8]) != 0):
+            continue
+        print(str([venue, line[4], int(line[7]), 1]))
+    except IndexError:
+        if(line[1] == "venue"):
+            venue = line[2]
