1d6e789dfc388c1ea5824dcdf66d33b186014351
Mayank Thakur <thakurmayank88@gmail.com>
2019-10-06 18:14:55 +0530

    Customized admin

--- adminmgr/api/admin.py
+++ adminmgr/api/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
 from .models import SubmissionAssignmentOne, Team, SubmissionAssignmentTwo
-# Register your models here.
 
+# admin.site.unregister(User)
+# admin.site.unregister(Group)
 admin.site.site_header = 'Big Data Assignment'
+
 
 class AssignmentAdmin(admin.ModelAdmin):
     list_display = ('id', 'team', 'submitted_on')
@@ -13,4 +15,3 @@ class AssignmentAdmin(admin.ModelAdmin):
 admin.site.register(SubmissionAssignmentTwo, AssignmentAdmin)
 admin.site.register(Team)
 admin.site.register(SubmissionAssignmentOne, AssignmentAdmin)
-# admin.site.unregister([Group, User])
