c3323bb31354fc44863d64d1a22535df03155d41
Ubuntu <ubuntu@ip-172-31-18-251.ap-south-1.compute.internal>
2019-10-11 16:01:26 +0000

    Random

---
+++ adminmgr/media/code/A2/python/task/BD_256_1126_1333_1347.py
@@ -0,0 +1,109 @@
+from __future__ import print_function
+
+import math
+def truncate(number, digits) -> float:
+    stepper = 10.0 ** digits
+    return math.trunc(stepper * number) / stepper
+
+import re
+import sys
+from operator import add
+
+from pyspark.sql import SparkSession
+
+
+def computeContribs(urls, rank):
+	num_urls = len(urls)
+	for url in urls:
+		yield (url, rank / num_urls)
+
+
+def parseNeighbors(urls):
+	parts = re.split(r',', urls)
+	bow_avg = float(parts[2])/float(parts[3])
+	return parts[1], bow_avg
+
+def parseNeighbors2(urls):
+	parts = re.split(r',', urls)
+	return parts[0], parts[1]
+
+def func(x):
+	if(x <= 0.0001):
+		return False
+	else:
+		return True
+
+
+if __name__ == "__main__":
+	if len(sys.argv) != 4:
+		print("Usage: pagerank <file> <iterations> <weight>", file=sys.stderr)
+		sys.exit(-1)
+
+	# Initialize the spark context.
+	spark = SparkSession\
+		.builder\
+		.appName("PythonPageRank")\
+		.getOrCreate()
+
+	lines = spark.read.text(sys.argv[1]).rdd.map(lambda r: r[0])
+	#print(lines.collect())
+
+	links = lines.map(lambda urls: parseNeighbors2(urls)).distinct().groupByKey().cache()
+
+	ranks_prev1 = lines.map(lambda urls: parseNeighbors(urls)).distinct().reduceByKey(lambda a,b: a+b).cache()
+
+	ranks_prev = ranks_prev1.mapValues(lambda x: x if x>1 else 1).cache()
+
+	#ranks_prev = links.map(lambda url_neighbors: (url_neighbors[0], 1.0))
+	i=1
+	c=0
+	if(int(sys.argv[2]) == 0):
+
+		if(int(sys.argv[3]) == 0):
+			weight = 0.80
+			
+		else:
+			weight = sys.argv[3]/100
+
+		while(i):
+				c=c+1
+
+				contribs = links.join(ranks_prev).flatMap(
+					lambda url_urls_rank: computeContribs(url_urls_rank[1][0], url_urls_rank[1][1]))
+
+				ranks_now = contribs.reduceByKey(add).join(ranks_prev).mapValues(lambda rank: (rank[0] * weight + (1-weight)))
+
+				diff = ranks_now.join(ranks_prev).mapValues(lambda x: x[1]-x[0])
+		 
+				diff_values = diff.values()
+		   
+				diff_nconverge = diff_values.filter(func)
+			   
+				empty = diff_nconverge.count()
+			 
+				if(empty == 0):
+					i=0
+				else:
+					ranks_prev = ranks_now
+			
+	else:
+
+		if(int(sys.argv[3]) == 0):
+			weight = 0.80
+			
+		else:
+			weight = int(sys.argv[3])/100
+
+		for iteration in range(int(sys.argv[2])):
+			contribs = links.join(ranks_prev).flatMap(
+				lambda url_urls_rank: computeContribs(url_urls_rank[1][0], url_urls_rank[1][1]))
+
+			ranks_now = contribs.reduceByKey(add).join(ranks_prev).mapValues(lambda rank: (rank[0] * weight + (1-weight)))
+			ranks_prev = ranks_now
+
+
+	for (link, rank) in ranks_now.sortByKey().sortBy(lambda a:-a[1]).collect():
+		print("%s, %.12f" % (link, float(rank)))
+	print(c)
+
+	spark.stop()
---
+++ adminmgr/media/code/A2/python/task/BD_72_206_262_704_L9VpQ0N.py
@@ -0,0 +1,48 @@
+import re
+import sys
+from operator import add
+from pyspark.sql import SparkSession
+import os
+
+#os.environ['PYSPARK_PYTHON'] = "/usr/bin/python3.6"
+old_ranks = list()
+ranks = list()
+
+def not_converged():
+    return old_ranks.join(ranks).map(lambda x : abs(x[1][0] - x[1][1]) > 0.0001).reduce(lambda x, y : x or y)     #return 1 if any have not converged
+
+def computeContribs(urls, rank):
+    num_urls = len(urls)
+    for url in urls:
+        yield(url, rank / num_urls)
+
+if __name__ == "__main__":
+    spark = SparkSession.builder.appName("BowlerRank").getOrCreate()
+    #lines = spark.read.text("file:///home/raunak/a2/BowlerRankTestData.txt").rdd.map(lambda r : r[0]).map(lambda urls : re.split(',+', urls))
+    lines = spark.read.text(sys.argv[1]).rdd.map(lambda r : r[0]).map(lambda urls : re.split(',+', urls))
+    
+    bowlingrates = lines.map(lambda x : (x[0], int(x[2]) / int(x[3])))
+    cumulativerates = bowlingrates.reduceByKey(lambda x, y : x + y)
+    ranks = cumulativerates.map(lambda x : (x[0], max(x[1], 1)))
+    
+    links = lines.map(lambda combo : (combo[0], combo[1])).distinct().groupByKey().cache()
+    #n_iters = int(sys.argv[1])
+    n_iters = int(sys.argv[2])
+    #calc_weight = int(sys.argv[2])
+    calc_weight = int(sys.argv[3])
+    
+    calc_weight = (calc_weight if calc_weight > 0 else 80) / 100
+    def_weight = 1 - calc_weight
+    
+    iter_count = 0
+    notconv = 1
+    while(notconv and ((iter_count < n_iters) if n_iters > 0 else True)):
+        old_ranks = ranks
+        contribs = links.join(old_ranks).flatMap(lambda person_opps_rank : computeContribs(person_opps_rank[1][0], person_opps_rank[1][1]))
+        ranks = contribs.reduceByKey(add).mapValues(lambda rank : rank * calc_weight + def_weight)
+        notconv = not_converged()
+        iter_count += 1
+        
+    for (player, rank) in ranks.sortBy(lambda x : (-x[1], x[0])).collect():
+        print("%s,%s" % (player, round(rank,12)))
+    spark.stop()
