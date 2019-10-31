09f63c6226c8555533800f0af2ba40ce7392b413
Mayank Thakur <thakurmayank88@gmail.com>
2019-10-31 18:36:53 +0530

    Put outputs

--- adminmgr/sparkstreammgr/executor.py
+++ adminmgr/sparkstreammgr/executor.py
@@ -3,6 +3,7 @@ from .config import *
 import errno
 import os
 import datetime
+import time
 
 
 def execute_task1(submission):
@@ -15,10 +16,13 @@ def execute_task1(submission):
     result_path = os.path.join(output_path, "1", "1")
     setters_path = os.path.join(
         BASE_PATH, SETTERS_BASE_PATH, "t1s1.txt")
+    setters_path_2 = os.path.join(
+        BASE_PATH, SETTERS_BASE_PATH, "t1s1_2.txt")
     os.makedirs(result_path, exist_ok=True)
     runcmd = "python3.6 "+submission.code_file_task_1.path + \
         " > "+result_path+"/t1s1.output"
     diffcmd = "diff -wBZ "+result_path+"/t1s1.output "+setters_path
+    diffcmd2 = "diff -wBZ "+result_path+"/t1s1.output "+setters_path_2
     try:
         run_code = os.system(runcmd)
         print("T1S1", runcmd)
@@ -28,9 +32,10 @@ def execute_task1(submission):
             scores.append(0)
         else:
             diff_code = os.system(diffcmd)
+            diff_code2 = os.system(diffcmd2)
             print("DT1S1", diffcmd)
             # diff_code = 0
-            if diff_code != 0:
+            if diff_code != 0 and diff_code2 != 0:
                 message += "Wrong Output<br>"
                 scores.append(0)
             else:
@@ -83,13 +88,17 @@ def execute_task2(submission):
     result_path = os.path.join(output_path, "2", "1")
     setters_path = os.path.join(
         BASE_PATH, SETTERS_BASE_PATH, "t2s1.txt")
+    setters_path_2 = os.path.join(
+        BASE_PATH, SETTERS_BASE_PATH, "t2s1_2.txt")
     os.makedirs(result_path, exist_ok=True)
     runappcmd = "python3 ~/app.py"
-    runcmd = "python3.6 "+submission.code_file_task_1.path +" 4 2" \
+    runcmd = "python3.6 "+submission.code_file_task_1.path + " 2 1" \
         " > "+result_path+"/t2s1.output"
     diffcmd = "diff -wBZ "+result_path+"/t2s1.output "+setters_path
+    diffcmd2 = "diff -wBZ "+result_path+"/t2s1.output "+setters_path_2
     try:
         run_app = os.system(runappcmd)
+        time.sleep(5)
         run_code = os.system(runcmd)
         print("T2S1", runcmd)
         # run_code = 0
@@ -98,9 +107,10 @@ def execute_task2(submission):
             scores.append(0)
         else:
             diff_code = os.system(diffcmd)
+            diff_code2 = os.system(diffcmd2)
             print("DT2S1", diffcmd)
             # diff_code = 0
-            if diff_code != 0:
+            if diff_code != 0 and diff_code2 != 0:
                 message += "Wrong Output<br>"
                 scores.append(0)
             else:
@@ -116,13 +126,17 @@ def execute_task2(submission):
     result_path = os.path.join(output_path, "2", "2")
     setters_path = os.path.join(
         BASE_PATH, SETTERS_BASE_PATH, "t2s2.txt")
+    setters_path_2 = os.path.join(
+        BASE_PATH, SETTERS_BASE_PATH, "t2s2_2.txt")
     os.makedirs(result_path, exist_ok=True)
     runappcmd = "python3 ~/app.py"
-    runcmd = "python3.6 "+submission.code_file_task_1.path +" 2 1" \
+    runcmd = "python3.6 "+submission.code_file_task_1.path + " 1 1" \
         " > "+result_path+"/t2s2.output"
     diffcmd = "diff -wBZ "+result_path+"/t2s2.output "+setters_path
+    diffcmd2 = "diff -wBZ "+result_path+"/t2s2.output "+setters_path_2
     try:
         run_app = os.system(runappcmd)
+        time.sleep(5)
         run_code = os.system(runcmd)
         print("T2S2", runcmd)
         # run_code = 0
@@ -131,9 +145,10 @@ def execute_task2(submission):
             scores.append(0)
         else:
             diff_code = os.system(diffcmd)
+            diff_code2 = os.system(diffcmd2)
             print("DT2S2", diffcmd)
             # diff_code = 0
-            if diff_code != 0:
+            if diff_code != 0 and diff_code2 != 0:
                 message += "Wrong Output<br>"
                 scores.append(0)
             else:
@@ -144,6 +159,7 @@ def execute_task2(submission):
         message += "Unknown Error <br>"
         scores.append(0)
     return [message, scores]
+
 
 def execute(submission_id):
     submission = SubmissionAssignmentThree.objects.get(id=submission_id)
